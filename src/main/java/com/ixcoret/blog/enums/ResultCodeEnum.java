9bcb5107a6748a64b6e724b3e303dfce6dea8897
ixcoret <song1036752344@163.com>
2021-10-07 21:59:24 +0800

    修改返回码描述

--- src/main/java/com/ixcoret/blog/enums/ResultCodeEnum.java
+++ src/main/java/com/ixcoret/blog/enums/ResultCodeEnum.java
@@ -16,10 +16,10 @@ public enum ResultCodeEnum {
     DATA_NOT_FOUND(40001, "未查到相关数据！"),
     PARAMS_ERROR(40002, "参数不正确！"),
     NOT_LOGIN(40003, "未登录或登录过期！"),
-    LOGIN_PARAMS_ERROR(40004, "用户名或密码不正确！"),
+    USERNAME_OR_PASSWORD_ERROR(40004, "用户名或密码不正确！"),
     DATA_DUPLICATE(40005, "数据库中已存在相关数据，禁止重复添加！"),
     REQUEST_METHOD_NOT_SUPPORTED(40006, "请求方式不正确！"),
-    JSON_FORMAT_ERROR(40007, "缺少json格式请求体参数或json格式不正确！"),
+    JSON_FORMAT_ERROR(40007, "缺少json格式的请求体参数或json格式不正确！"),
     ARGUMENT_TYPE_NOT_MATCH(40008, "请求参数数据类型不匹配")
     ;
 
