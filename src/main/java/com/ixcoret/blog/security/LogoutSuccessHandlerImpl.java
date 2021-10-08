46dafaea35aabe045fbf763b1b6ecf9971e593e3
ixcoret <song1036752344@163.com>
2021-10-08 22:03:28 +0800

    添加注销登录成功处理

--- src/main/java/com/ixcoret/blog/security/LogoutSuccessHandlerImpl.java
+++ src/main/java/com/ixcoret/blog/security/LogoutSuccessHandlerImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.ixcoret.blog.api.Result;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,15 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 退出失败处理
+ * 注销登录成功处理
  *
  * @author ixcoret
  * @createTime 2021/10/5 17:20
  */
+@Component
 public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(JSON.toJSONString(Result.success("退出登录")));
+        response.getWriter().write(JSON.toJSONString(Result.success("退出登录成功")));
     }
 }
