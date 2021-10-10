ccbf9916aa7be9bfb3526d0d680e39ac0f09b89d
ixcoret <song1036752344@163.com>
2021-10-10 23:27:43 +0800

    实现获取当前登录用户信息的静态方法

--- src/main/java/com/ixcoret/blog/context/SystemContext.java
+++ src/main/java/com/ixcoret/blog/context/SystemContext.java
@@ -2,6 +2,8 @@ package com.ixcoret.blog.context;
 
 import com.ixcoret.blog.entity.OperationLog;
 import lombok.Data;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 
 /**
  * 解决多线程之间数据隔离问题
@@ -12,7 +14,12 @@ import lombok.Data;
 public class SystemContext {
     private static ThreadLocal<OperationLog> threadLocal = new ThreadLocal<>();
 
-    public static OperationLog getLog() {
+    /**
+     * 获取当前线程的操作日志对象
+     *
+     * @return
+     */
+    public static OperationLog getOperationLog() {
         if (threadLocal.get() == null) {
             threadLocal.set(new OperationLog());
         }
@@ -21,5 +28,14 @@ public class SystemContext {
 
     public static void remove() {
         threadLocal.remove();
+    }
+
+    /**
+     * 获取当前登录用户
+     *
+     * @return 用户登录信息
+     */
+    public static User getLoginUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
